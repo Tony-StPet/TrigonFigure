3bed38604a4de529e2dc44cd63503c93681d587a
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-23 21:36:02 +0300

    переработан треугольник и его методы

--- src/figure/Triangle.java
+++ src/figure/Triangle.java
@@ -1,30 +1,25 @@
 package figure;
 
+// наследование от фигуры, беру периметр и площадь как abstract
 public class Triangle extends Figure{
     Point a;     Point b;     Point c;
 
-
+// конструктор
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
+    // функция для поиска периметра треугольника по его точкам
     public double perimeter(){
-
         double ab = a.distanceTo(b);
         double bc = b.distanceTo(c);
         double ca = c.distanceTo(a);
         return ab + bc +ca;
     }
-    public double calcPer(){
 
-    double ab = a.distanceTo(b);
-    double bc = b.distanceTo(c);
-    double ca = c.distanceTo(a);
-            return ab + bc +ca;
-        }
-
+    // функция для поиска площади треугольника по его точкам
     public double area(){
         double ab = a.distanceTo(b);
         double bc = b.distanceTo(c);
@@ -34,13 +29,15 @@ public class Triangle extends Figure{
     }
 
 
-
+    // функция вывода toString
     @Override
     public String toString() {
         return "Figure.Triangle{" +
                 "a=" + a +  ", b=" + b + ", c=" + c + '}';
     }
 
+    // функция для проверки треугольника на то что он равнобедренный
+    // т.е. две его стороны раны, и добавляю погрешность tolerance
     public boolean ifTrianIsosceles()
     {
         double tolerance = 0.01;
@@ -56,6 +53,9 @@ public class Triangle extends Figure{
             return true;
             else return false;
     }
+
+    // функция для проверки треугольника на то что он прямоугольный
+    // использую теорему пифагора и добавляю погрешность tolerance
     public boolean ifTrianRectangular(){
         double tolerance = 0.01;
         double ab = a.distanceTo(b);
@@ -66,6 +66,8 @@ public class Triangle extends Figure{
         if (bc*bc+ca*ca-ab*ab<= tolerance) return true;
             else return false;
     }
+
+
 
 }
 
